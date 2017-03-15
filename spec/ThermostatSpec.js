14d0b5e523b1d7c75d988e4bac01e6ba8d0b1c98
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-15 12:49:22 +0000

    refactor previous commit

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,17 +1,27 @@
 // Thermostat starts at 20 degrees
+// Thermostat starts at 20 degrees
 
 describe ("Thermostat", function() {
 
+  var thermostat
+
+  beforeEach(function(){
+    thermostat = new Thermostat;
+  })
+
   describe ("exists", function() {
 
+    console.log(thermostat);
     it ("defined", function() {
-      var thermostat = new Thermostat
       expect(thermostat).toBeDefined();
     });
 
     it ("has a default temperature of 20 degrees", function() {
-      var thermostat = new Thermostat
       expect(thermostat.defaultTemperature).toEqual(20)
+    });
+
+    it ("has a minimum temperature of 10 degrees", function () {
+      expect(thermostat.minimumTemperature).toEqual(10)
     });
 
   });
