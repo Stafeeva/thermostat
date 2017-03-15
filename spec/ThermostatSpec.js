173702960c1d0e7ba346f49ff973fb451481d072
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-15 12:28:39 +0000

    add Thermostat object and test for default temperature

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,12 +1,19 @@
 // Thermostat starts at 20 degrees
 
-describe ("thermostat", function() {
+describe ("Thermostat", function() {
 
   describe ("exists", function() {
 
     it ("defined", function() {
+      var thermostat = new Thermostat
       expect(thermostat).toBeDefined();
     });
+
+    it ("has a default temperature of 20 degrees", function() {
+      var thermostat = new Thermostat
+      expect(thermostat.defaultTemperature).toEqual(20)
+    });
+
   });
 
 });
