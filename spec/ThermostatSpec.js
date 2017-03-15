f66f30f802f392c785f4f82a2eed9a245e1d8987
Natalia Stafeeva <n.stafeeva@gmail.com>
2017-03-15 12:19:43 +0000

    Wrote test for thermostat object

---
+++ spec/ThermostatSpec.js
@@ -0,0 +1,12 @@
+// Thermostat starts at 20 degrees
+
+describe ("thermostat", function() {
+
+  describe ("exists", function() {
+
+    it ("defined", function() {
+      expect(thermostat).toBeDefined();
+    });
+  });
+
+});
